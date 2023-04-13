fix metric matrix derivative

src/latent_geometry/metric/abstract.py
@@ -145,6 +145,10 @@ class MappingPullbackMetric(PullbackMetric, ABC):
         return J.T @ A @ J
 
     def metric_matrix_derivative(self, base_point: np.ndarray) -> np.ndarray:
+        J = self.mapping.jacobian(base_point)
         H = self.mapping.hessian(base_point)
         A = self.ambient_metric.metric_matrix(base_point)
-        return np.einsum("rs,rik,sjk->ijk", A, H, H)
+
+        term_1 = np.einsum("rs,rik,sj->ijk", A, H, J)
+        term_2 = np.einsum("rs,sjk,ri->ijk", A, H, J)
+        return term_1 + term_2