rename hessian -> 2nd derivative; fix base point images

src/latent_geometry/mapping/abstract.py
@@ -38,8 +38,8 @@ class Mapping(ABC):
         """
 
     @abstractmethod
-    def hessian(self, z: np.ndarray) -> np.ndarray:
-        r"""Compute mapping's hessian tensor.
+    def second_derivative(self, z: np.ndarray) -> np.ndarray:
+        r"""Compute mapping's second derivative tensor.
 
         Parameters
         ----------
@@ -49,8 +49,8 @@ class Mapping(ABC):
         Returns
         -------
         H : (D', D, D) ndarray
-            Hessian of the mapping computed at z, where indices
-            of the derivation are put last.
+            The second derivative of the mapping computed at z,
+            where indices of the derivation are put last.
 
             :math: `H_{ijk} = \partial_{jk} g_i`.
         """

src/latent_geometry/mapping/sphere_immersion.py
@@ -25,7 +25,7 @@ class SphereImmersion(Mapping):
         z_tensor = torch.tensor(z)
         return jacrev(self.immerse)(z_tensor).numpy()
 
-    def hessian(self, z: np.ndarray) -> np.ndarray:
+    def second_derivative(self, z: np.ndarray) -> np.ndarray:
         z_tensor = torch.tensor(z)
         return jacrev(jacrev(self.immerse))(z_tensor).numpy()
 

src/latent_geometry/mapping/torch.py
@@ -22,10 +22,10 @@ class TorchModelMapping(Mapping):
         jacobian_torch = jacrev(self._call_flat_model)(z_torch)
         return self._to_numpy(jacobian_torch)
 
-    def hessian(self, z: np.ndarray) -> np.ndarray:
+    def second_derivative(self, z: np.ndarray) -> np.ndarray:
         z_torch = self._to_torch(z)
-        hessian_torch = jacfwd(jacrev(self._call_flat_model))(z_torch)
-        return self._to_numpy(hessian_torch)
+        second_derivative_torch = jacfwd(jacrev(self._call_flat_model))(z_torch)
+        return self._to_numpy(second_derivative_torch)
 
     def _call_flat_model(self, x: torch.Tensor) -> torch.Tensor:
         """Reshapes data so that we can pretend that model's input and output is 1D."""

src/latent_geometry/metric/abstract.py
@@ -140,14 +140,16 @@ class MappingPullbackMetric(PullbackMetric, ABC):
         """Map from latent to ambient space."""
 
     def metric_matrix(self, base_point: np.ndarray) -> np.ndarray:
+        ambient_point = self.mapping(base_point)
         J = self.mapping.jacobian(base_point)
-        A = self.ambient_metric.metric_matrix(base_point)
+        A = self.ambient_metric.metric_matrix(ambient_point)
         return J.T @ A @ J
 
     def metric_matrix_derivative(self, base_point: np.ndarray) -> np.ndarray:
+        ambient_point = self.mapping(base_point)
         J = self.mapping.jacobian(base_point)
-        H = self.mapping.hessian(base_point)
-        A = self.ambient_metric.metric_matrix(base_point)
+        H = self.mapping.second_derivative(base_point)
+        A = self.ambient_metric.metric_matrix(ambient_point)
 
         term_1 = np.einsum("rs,rik,sj->ijk", A, H, J)
         term_2 = np.einsum("rs,sjk,ri->ijk", A, H, J)