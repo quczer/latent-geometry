5b9de4a56ac72018ba35eb7922925c1238fbad22
quczer <kucharczi@gmail.com>
2023-04-16 20:17:48 +0200

    latent maniofold

---
+++ src/latent_geometry/manifold/latent.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+from latent_geometry.manifold.abstract import Manifold
+from latent_geometry.mapping.abstract import Mapping
+from latent_geometry.metric.abstract import Metric
+from latent_geometry.metric.manifold import ManifoldMetric
+from latent_geometry.path import Path
+from latent_geometry.solver.exponential import IVPExponentialSolver
+from latent_geometry.solver.logarithm import BVPLogarithmSolver
+
+
+class LatentManifold(Manifold):
+    def __init__(self, mapping: Mapping, ambient_metric: Metric):
+        self.metric = ManifoldMetric(mapping, ambient_metric)
+        self._exp_solver = IVPExponentialSolver()
+        self._log_solver = BVPLogarithmSolver()
+
+    def geodesic(self, z_a: np.ndarray, z_b: np.ndarray) -> Path:
+        path = self._log_solver.find_path(z_a, z_b, self.metric.acceleration)
+        return path
+
+    def path_given_direction(self, z: np.ndarray, velocity_vec: np.ndarray) -> Path:
+        path = self._exp_solver.mark_path(z, velocity_vec, self.metric.acceleration)
+        return path
