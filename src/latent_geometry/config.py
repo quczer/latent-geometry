6037a2ea3ba29cb755be8dacf4c8a30011c47008
quczer <kucharczi@gmail.com>
2023-07-12 14:52:21 +0200

    add dvc folders to config

--- src/latent_geometry/config.py
+++ src/latent_geometry/config.py
@@ -2,3 +2,5 @@ from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent.parent.parent.resolve()
 NOTEBOOKS_DIR = ROOT_DIR / "notebooks"
+DATA_DIR = ROOT_DIR / "data"
+MODELS_DIR = ROOT_DIR / "models"
