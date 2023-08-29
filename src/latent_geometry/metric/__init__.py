cd4b9e32c4377579f27ea6c19050a67f85ddcdb4
quczer <kucharczi@gmail.com>
2023-08-29 12:18:03 +0200

    solver + investigations

--- src/latent_geometry/metric/__init__.py
+++ src/latent_geometry/metric/__init__.py
@@ -1,5 +1,12 @@
 from latent_geometry.metric.abstract import Metric
 from latent_geometry.metric.euclidean import EuclideanMetric
 from latent_geometry.metric.manifold import EuclideanPullbackMetric, ManifoldMetric
+from latent_geometry.metric.pullback import PullbackMetric
 
-__all__ = ["Metric", "EuclideanMetric", "EuclideanPullbackMetric", "ManifoldMetric"]
+__all__ = [
+    "Metric",
+    "EuclideanMetric",
+    "PullbackMetric",
+    "EuclideanPullbackMetric",
+    "ManifoldMetric",
+]
