736d35a1e5465d1d37353a222b22255525b5aa54
quczer <kucharczi@gmail.com>
2023-09-06 13:48:53 +0200

    model which does not work

--- src/latent_geometry/model/mnist_vae.py
+++ src/latent_geometry/model/mnist_vae.py
@@ -1,3 +1,5 @@
+from typing import OrderedDict
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -6,128 +8,158 @@ from latent_geometry.config import MODELS_DIR
 
 
 class EncoderVAE(nn.Module):
-    def __init__(
-        self, init_channels: int, latent_dim: int, kernel_size: int, image_channels: int
-    ):
-        super(EncoderVAE, self).__init__()
+    def __init__(self, init_channels: int = 8, latent_dim: int = 2):
+        super().__init__()
+        self.convs = nn.Sequential(
+            OrderedDict(
+                [
+                    (
+                        "conv1",
+                        nn.Conv2d(
+                            in_channels=1,
+                            out_channels=init_channels,
+                            kernel_size=4,
+                            stride=2,
+                            padding=1,
+                        ),
+                    ),
+                    ("tanh1", nn.Tanh()),
+                    (
+                        "conv2",
+                        nn.Conv2d(
+                            in_channels=init_channels,
+                            out_channels=init_channels * 2,
+                            kernel_size=4,
+                            stride=2,
+                            padding=1,
+                        ),
+                    ),
+                    ("tanh2", nn.Tanh()),
+                    (
+                        "conv3",
+                        nn.Conv2d(
+                            in_channels=init_channels * 2,
+                            out_channels=init_channels * 4,
+                            kernel_size=4,
+                            stride=2,
+                            padding=1,
+                        ),
+                    ),
+                    ("tanh3", nn.Tanh()),
+                    (
+                        "conv4",
+                        nn.Conv2d(
+                            in_channels=init_channels * 4,
+                            out_channels=64,
+                            kernel_size=4,
+                            stride=2,
+                            padding=0,
+                        ),
+                    ),
+                    ("tanh4", nn.Tanh()),
+                ]
+            )
+        )
+        self.fcs = nn.Sequential(
+            OrderedDict(
+                [
+                    ("fc1", nn.Linear(64, 128)),
+                    ("tanh", nn.Tanh()),
+                    ("fc2", nn.Linear(128, latent_dim + 1)),
+                ]
+            )
+        )
 
-        # encoder
-        self.enc1 = nn.Conv2d(
-            in_channels=image_channels,
-            out_channels=init_channels,
-            kernel_size=kernel_size,
-            stride=2,
-            padding=1,
-        )
-        self.enc2 = nn.Conv2d(
-            in_channels=init_channels,
-            out_channels=init_channels * 2,
-            kernel_size=kernel_size,
-            stride=2,
-            padding=1,
-        )
-        self.enc3 = nn.Conv2d(
-            in_channels=init_channels * 2,
-            out_channels=init_channels * 4,
-            kernel_size=kernel_size,
-            stride=2,
-            padding=1,
-        )
-        self.enc4 = nn.Conv2d(
-            in_channels=init_channels * 4,
-            out_channels=64,
-            kernel_size=kernel_size,
-            stride=2,
-            padding=0,
-        )
-        # fully connected layers for learning representations
-        self.fc1 = nn.Linear(64, 128)
-        self.fc_mu = nn.Linear(128, latent_dim)
-        self.fc_log_var = nn.Linear(128, latent_dim)
+    # def sample(self, x: torch.Tensor) -> torch.Tensor:
+    #     """sample from posterior distribution"""
+    #     mu, std = self.forward(x)
+    #     noise = torch.randn_like(std)
+    #     sample = mu + (noise * std)
+    #     return sample
 
-    def reparameterize(self, mu, log_var):
-        """
-        :param mu: mean from the encoder's latent space
-        :param log_var: log variance from the encoder's latent space
-        """
-        std = torch.exp(0.5 * log_var)  # standard deviation
-        eps = torch.randn_like(std)  # `randn_like` as we need the same size
-        sample = mu + (eps * std)  # sampling
-        return sample
+    def forward(self, x: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+        """Return (mu, std), where std is a batch of scalars"""
+        if x.dim() < 4:
+            x = x.unsqueeze(0)
+        x = self.convs(x)
+        x = x.squeeze(2, 3)
+        hidden = self.fcs(x)
 
-    def forward(self, x):
-        # encoding
-        x = F.relu(self.enc1(x))
-        x = F.relu(self.enc2(x))
-        x = F.relu(self.enc3(x))
-        x = F.relu(self.enc4(x))
-        batch, _, _, _ = x.shape
-        x = F.adaptive_avg_pool2d(x, 1).reshape(batch, -1)
-        hidden = self.fc1(x)
-        # get `mu` and `log_var`
-        mu = self.fc_mu(hidden)
-        log_var = self.fc_log_var(hidden)
-        # get the latent vector through reparameterization
-        z = self.reparameterize(mu, log_var)
-        return z, mu, log_var
+        mu, std_out = hidden[..., :-1], hidden[..., [-1]]
+        std = F.softplus(std_out)
+        return mu, std
 
 
 class DecoderVAE(nn.Module):
-    def __init__(
-        self, init_channels: int, latent_dim: int, kernel_size: int, image_channels: int
-    ):
-        super(DecoderVAE, self).__init__()
+    def __init__(self, init_channels: int = 8, latent_dim: int = 2):
+        super().__init__()
 
-        # decoder
-        self.fc2 = nn.Linear(latent_dim, 64)
-
-        self.dec1 = nn.ConvTranspose2d(
-            in_channels=64,
-            out_channels=init_channels * 8,
-            kernel_size=kernel_size,
-            stride=1,
-            padding=0,
+        self.fcs = nn.Sequential(
+            OrderedDict([("fc", nn.Linear(latent_dim, 64)), ("tanh", nn.Tanh())])
         )
-        self.dec2 = nn.ConvTranspose2d(
-            in_channels=init_channels * 8,
-            out_channels=init_channels * 4,
-            kernel_size=kernel_size,
-            stride=2,
-            padding=1,
+        self.deconvs = nn.Sequential(
+            OrderedDict(
+                [
+                    (
+                        "deconv1",
+                        nn.ConvTranspose2d(
+                            in_channels=64,
+                            out_channels=init_channels * 8,
+                            kernel_size=4,
+                            stride=2,
+                            padding=0,
+                        ),
+                    ),
+                    ("tanh1", nn.Tanh()),
+                    (
+                        "deconv2",
+                        nn.ConvTranspose2d(
+                            in_channels=init_channels * 8,
+                            out_channels=init_channels * 4,
+                            kernel_size=4,
+                            stride=2,
+                            padding=1,
+                        ),
+                    ),
+                    ("tanh2", nn.Tanh()),
+                    (
+                        "deconv3",
+                        nn.ConvTranspose2d(
+                            in_channels=init_channels * 4,
+                            out_channels=init_channels * 2,
+                            kernel_size=4,
+                            stride=2,
+                            padding=1,
+                        ),
+                    ),
+                    ("tanh3", nn.Tanh()),
+                    (
+                        "deconv4",
+                        nn.ConvTranspose2d(
+                            in_channels=init_channels * 2,
+                            out_channels=1,
+                            kernel_size=4,
+                            stride=2,
+                            padding=1,
+                        ),
+                    ),
+                    ("sigmoid", nn.Sigmoid()),
+                ]
+            )
         )
-        self.dec3 = nn.ConvTranspose2d(
-            in_channels=init_channels * 4,
-            out_channels=init_channels * 2,
-            kernel_size=kernel_size,
-            stride=2,
-            padding=1,
-        )
-        self.dec4 = nn.ConvTranspose2d(
-            in_channels=init_channels * 2,
-            out_channels=image_channels,
-            kernel_size=kernel_size,
-            stride=2,
-            padding=1,
-        )
 
-    def decode(self, z):
-        # decoding
-        z = self.fc2(z)
-        z = z.view(-1, 64, 1, 1)
-        x = F.relu(self.dec1(z))
-        x = F.relu(self.dec2(x))
-        x = F.relu(self.dec3(x))
-        reconstruction = torch.sigmoid(self.dec4(x))
-        return reconstruction  # , mu, log_var
-
-    def forward(self, z):
-        # z = self.encode(x)
-        return self.decode(z)
+    def forward(self, z: torch.Tensor) -> torch.Tensor:
+        if z.dim() == 1:
+            z = z.unsqueeze(0)
+        z = self.fcs(z)
+        z = z.reshape((*z.shape, 1, 1))
+        z = self.deconvs(z)
+        return z
 
 
-def load_decoder(device: torch.device = torch.device("cpu")) -> DecoderVAE:
-    return torch.load(MODELS_DIR / "mnist" / "decoder.pt", map_location=device)
+# def load_decoder(device: torch.device = torch.device("cpu")) -> DecoderVAE:
+#     return torch.load(MODELS_DIR / "mnist" / "decoder.pt", map_location=device)
 
 
-def load_encoder(device: torch.device = torch.device("cpu")) -> EncoderVAE:
-    return torch.load(MODELS_DIR / "mnist" / "encoder.pt", map_location=device)
+# def load_encoder(device: torch.device = torch.device("cpu")) -> EncoderVAE:
+#     return torch.load(MODELS_DIR / "mnist" / "encoder.pt", map_location=device)
