628a82167a8b45d11c735a63f7202fdea4e45bea
quczer <kucharczi@gmail.com>
2023-10-18 11:01:21 +0200

    improve input optimizer

--- src/latent_geometry/optimizer.py
+++ src/latent_geometry/optimizer.py
@@ -1,5 +1,5 @@
 import math
-from typing import Callable
+from typing import Callable, Literal
 
 import torch
 import torch.optim.sgd
@@ -12,13 +12,21 @@ class InputSGDOptimizer:
         param: torch.Tensor,
         mapping: Callable[[torch.Tensor], torch.Tensor],
         lr: float,
+        gradient_type: Literal["standard", "geometric"] = "standard",
+        normalize_gradient: bool = True,
     ):
         if lr < 0.0:
-            raise ValueError("Invalid learning rate: {}".format(lr))
+            raise ValueError(f"Invalid learning rate: {lr}")
+        if gradient_type not in ("standard", "geometric"):
+            raise ValueError(f"Invalid gradient_type: {gradient_type}")
         assert isinstance(param, torch.Tensor)
+        assert isinstance(normalize_gradient, bool)
+
         self.param = param
         self.mapping = mapping
         self.lr = lr
+        self.gradient_type = gradient_type
+        self.normalize_gradient = normalize_gradient
 
     def step(self, closure=None):
         loss = None
@@ -27,11 +35,20 @@ class InputSGDOptimizer:
                 loss = closure()
 
         with torch.no_grad():
-            shape = self.param.shape
-            g_inv_flat = self._calc_inv_matrix_flat(self.param, self.mapping)
-            true_grad_flat = torch.mv(g_inv_flat, self.param.grad.reshape(-1))
-            true_grad = true_grad_flat.reshape(shape)
-            self.param.add_(true_grad, alpha=-self.lr)
+            if self.gradient_type == "geometric":
+                shape = self.param.shape
+                g_flat = self._calc_metric_matrix_flat(self.param, self.mapping)
+                grad_flat = torch.mv(g_flat, self.param.grad.reshape(-1))
+                grad = grad_flat.reshape(shape)
+
+                if self.normalize_gradient:
+                    grad.mul_(
+                        torch.linalg.norm(self.param.grad) / torch.linalg.norm(grad)
+                    )
+            else:
+                grad = self.param.grad
+
+            self.param.add_(grad, alpha=-self.lr)
 
         return loss
 
@@ -39,7 +56,7 @@ class InputSGDOptimizer:
         self.param.grad = None
 
     @staticmethod
-    def _calc_inv_matrix_flat(
+    def _calc_metric_matrix_flat(
         x: torch.Tensor, mapping: Callable[[torch.Tensor], torch.Tensor]
     ) -> torch.Tensor:
         shape = x.shape
